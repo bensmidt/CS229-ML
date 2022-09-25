8f92ed1f0f80f0d7396250a6f89a18007c1e3c27
Benjamin Smidt <benjamin.raymond.smidt@gmail.com>
2022-09-25 11:56:50 -0500

    fixing merge

--- ProbSet2/src/p01_lr.py
+++ ProbSet2/src/p01_lr.py
@@ -1,14 +1,8 @@
 # Important note: you do not have to modify this file for your homework.
-<<<<<<< HEAD
 import os
 import util
 import numpy as np
 from matplotlib import pyplot as plt
-=======
-
-import util
-import numpy as np
->>>>>>> 14d06c46a874bccc9da5d01eb803eb6f3aff1ce2
 
 
 def calc_grad(X, Y, theta):
@@ -17,11 +11,7 @@ def calc_grad(X, Y, theta):
 
     margins = Y * X.dot(theta)
     probs = 1. / (1 + np.exp(margins))
-<<<<<<< HEAD
     grad = -(1./m) * (X.T.dot(probs * Y)) + 0.0001 * np.linalg.norm(theta) # Regularization term
-=======
-    grad = -(1./m) * (X.T.dot(probs * Y))
->>>>>>> 14d06c46a874bccc9da5d01eb803eb6f3aff1ce2
 
     return grad
 
@@ -30,18 +20,13 @@ def logistic_regression(X, Y):
     """Train a logistic regression model."""
     m, n = X.shape
     theta = np.zeros(n)
-<<<<<<< HEAD
     learning_rate = 1
-=======
-    learning_rate = 10
->>>>>>> 14d06c46a874bccc9da5d01eb803eb6f3aff1ce2
 
     i = 0
     while True:
         i += 1
         prev_theta = theta
         grad = calc_grad(X, Y, theta)
-<<<<<<< HEAD
 
         # learning_rate /= i**2
 
@@ -52,11 +37,6 @@ def logistic_regression(X, Y):
             print('Training loss = %f' % np.mean(np.log(1 + np.exp(-Y * X.dot(theta)))))
             
             print('||theta_k - theta_{k-1}|| = %f' % np.linalg.norm(prev_theta - theta))
-=======
-        theta = theta - learning_rate * grad
-        if i % 10000 == 0:
-            print('Finished %d iterations' % i)
->>>>>>> 14d06c46a874bccc9da5d01eb803eb6f3aff1ce2
         if np.linalg.norm(prev_theta - theta) < 1e-15:
             print('Converged in %d iterations' % i)
             break
@@ -64,7 +44,6 @@ def logistic_regression(X, Y):
 
 
 def main():
-<<<<<<< HEAD
     data_path = os.path.join(os.path.dirname(__file__), '..', 'data')
     train_path_a = os.path.join(data_path, 'ds1_a.csv')
     train_path_b = os.path.join(data_path, 'ds1_b.csv')
@@ -95,20 +74,8 @@ def main():
     # Logistic regression dataset B
     print('\n==== Training model on data set B ====')
     Xb, Yb = util.load_csv(train_path_b, add_intercept=True)
-=======
-    print('==== Training model on data set A ====')
-    Xa, Ya = util.load_csv('../data/ds1_a.csv', add_intercept=True)
-    logistic_regression(Xa, Ya)
-
-    print('\n==== Training model on data set B ====')
-    Xb, Yb = util.load_csv('../data/ds1_b.csv', add_intercept=True)
->>>>>>> 14d06c46a874bccc9da5d01eb803eb6f3aff1ce2
     logistic_regression(Xb, Yb)
 
 
 if __name__ == '__main__':
-<<<<<<< HEAD
     main()
-=======
-    main()
->>>>>>> 14d06c46a874bccc9da5d01eb803eb6f3aff1ce2
